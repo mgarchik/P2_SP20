9cbe0814e95f98d4eefe81a1ecba4f1537ed5ac8
garch <mgarchik@fwparker.org>
2020-01-28 14:24:31 -0600

    First Commit

---
+++ Notes/01_modules.py
@@ -0,0 +1,16 @@
+#  MODULES, IMPORTS AND LIBRARIES
+import turtle
+
+from math import pi, cos  # import single items from a lib
+from random import *  # imports everything locally
+import pandas as pd
+import import_me
+
+if __name__ == "__main__":
+    # This code only runs when you run THIS FILE first
+    print("Hello")
+    # my_turtle = turtle.Turtle()
+    print(pi)
+    print(cos(0.5))
+    print(randrange(1, 7))
+    print(import_me.x)
---
+++ Notes/import_me.py
@@ -0,0 +1,6 @@
+
+x = 5
+y = 10
+
+if __name__ == "__main__":
+    print("x + y =", x + y)
