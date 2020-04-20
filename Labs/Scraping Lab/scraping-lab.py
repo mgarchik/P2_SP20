2598a65db31b1103f7307aea03978083214cc43e
garch <mgarchik@fwparker.org>
2020-04-20 09:01:41 -0500

    routine

--- Labs/Scraping Lab/scraping-lab.py
+++ Labs/Scraping Lab/scraping-lab.py
@@ -1,14 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+
 # SCRAPING PROBLEMS
 # Twitter Scraping (15pts)
 # Go to your favorite follow on Twitter.  (not someone who posts explicit materials please)
 # Inspect the twitter feed in Chrome.
 # You'll notice that the tweets are stored in a ordered list <ol></ol>, and individual tweets are contained as list items <li></li>.
-# Use BeautifulSoup and requests to grab the text contents of last 5 tweetslocated on the twitter page you chose.
+# Use BeautifulSoup and requests to grab the text contents of last 5 tweets located on the twitter page you chose.
 # Print the tweets in a nicely formatted way.
 # Have fun.  Again, nothing explicit.
 
 #  print("{} {}!".format("Hello", "World"))
+print("{} {}!".format("Hello", "World"))
 
+twit_url = "https://twitter.com/arielhelwani"
+twit_page = requests.get(twit_url)
+
+twit_soup = BeautifulSoup(twit_page.text, "html.parser")
+print(twit_soup.prettify())
+
+# tweets = twit_soup.find_all(aria_label="Timeline: Ariel Helwani’s Tweets", class_="css-1dbjc4n")
+# tweet_divs = twit_soup.find_all(id="accessible-list-2")
+tweet_divs = twit_soup.find_all("div", class_="tweet")
+print(tweet_divs[0].text)
+
+
+for i in range(5):
+    print(tweet_divs[i].text)
 
 # Weather Scraping (15pts)
 # Below is a link to a 10-day weather forecast at weather.com
@@ -23,5 +41,12 @@
 # Wednesday, April 4 will be Partly Cloudy/Windy with a High of 37 degrees and a low of 25, humidity at 52%.  There is 0% chance of rain with winds out of the WNW at 22 mph.
 # if the sentence is a little different than shown, that will work; do what you can.  Don't forget about our friend string.format()
 
+print("{} {}!".format("Hello", "World"))
+
+weather_url = "https://weather.com/weather/tenday/l/368d5a4e7d156c3f508cb653f95018ec2747fdacb01e8d8d17386237f84962ac"
+weather_page = requests.get(weather_url)
+
+weather_soup = BeautifulSoup(weather_page.text, "html.parser")
+print(weather_soup.prettify())
 
 
